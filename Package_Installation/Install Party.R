1dc04f0869e535e4356ca8055c9a96599d6bcdae
Giovanni Pietro Vitali <giovvi@gmail.com>
2021-07-30 11:37:37 +0200

    Add files via upload

---
+++ Package_Installation/Install Party.R
@@ -0,0 +1,46 @@
+# #################################################################################### #
+## Once installed R and RStudio, you will also have to install a series of R packages. 
+## To do it, write in the console install.packages(“new_package”)                      
+## ...and press Enter. 
+## A series of operations will display in the console, 
+## ending up with a message that confirms the installation of the package. 
+
+# #################################################################################### #
+
+## In Distant Reading in R workshop we will use the following packages.
+## To install each of them go on each line and to run the command
+## press ctrl (on PC) or cmd (on Mac) and enter. 
+## Then wait the message that confirms the installation of the package.
+
+## Pay attention, some packages required a confirmation during the install process
+
+install.packages("caret")
+install.packages("e1071")
+install.packages("ggplot2")
+install.packages("ggraph")
+install.packages("ggwordcloud")
+install.packages("htmltools")
+install.packages("htmlwidgets")
+install.packages("igraph")
+install.packages("lattice")
+install.packages("leafem")
+install.packages("leaflet")
+install.packages("leaflet.extras")
+install.packages("leaflet.minicharts")
+install.packages("leafpop")
+install.packages("mallet")
+install.packages("mapview")
+install.packages("network")
+install.packages("networkD3")
+install.packages("quanteda")
+install.packages("raster")
+install.packages("RColorBrewer")
+install.packages("rgdal")
+install.packages("sf")
+install.packages("sp")
+install.packages("stylo")
+install.packages("syuzhet")
+install.packages("tidyverse")
+install.packages("topicmodels")
+install.packages("udpipe")
+install.packages("word2vec")
