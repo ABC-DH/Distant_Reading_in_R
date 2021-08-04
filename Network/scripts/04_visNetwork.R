cc737f9db87b7079805cf9dc01fda2b4cc514e3f
Giovanni Pietro Vitali <giovvi@gmail.com>
2021-08-04 14:15:15 +0200

    Add files via upload

---
+++ Network/scripts/04_visNetwork.R
@@ -0,0 +1,31 @@
+################
+## visNetwork ##
+################
+
+## More information https://datastorm-open.github.io/visNetwork/
+
+library(visNetwork)
+library(tidyverse)
+
+# Load data
+nodes <- read_csv("data/nodes.csv")
+edges <- read_csv("data/edges.csv")
+
+# Simple interactive plot
+visNetwork(nodes, 
+           edges,
+           width = "100%")
+
+# Width attribute
+edges <- mutate(edges, 
+                width = weight/5 + 1)
+
+# visNetwork edge width plot
+visNetwork(nodes, 
+           edges, 
+           width = "100%") %>% 
+  
+  visIgraphLayout(layout = "layout_with_fr") %>% 
+  
+  visEdges(arrows = "middle")
+
