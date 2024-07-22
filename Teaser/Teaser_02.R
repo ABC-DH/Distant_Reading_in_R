e038e2a3cc8f00b22ec121a7bfc61061154e2868
Giovanni Pietro Vitali <giovvi@gmail.com>
2024-07-22 17:29:06 +0200

    Add files via upload

--- Teaser/Teaser_02.R
+++ Teaser/Teaser_02.R
@@ -6,7 +6,7 @@ install.packages("visNetwork")
 install.packages("tidyverse")
 install.packages("leaflet")
 install.packages("sp")
-install.packages("rgdal")
+install.packages("sf")
 install.packages("igraph")
 
 ################################################################################
@@ -61,63 +61,66 @@ visNetwork(nodes,
 ##################### MAPS #####################################################
 ################################################################################
 
+# Load the required libraries for mapping and spatial data manipulation
 library(leaflet)
 library(sp)
-library(rgdal)
+library(sf)
 
-## Load the shapefile and name it
-countries <- readOGR('Mapping/data/shp/countries/countries.shp')
+# Load the shapefile and name it 'countries'
+countries <- st_read('Mapping/data/shp/countries/countries-polygon.shp')
 
-## Read the csv
+# Read the CSV file containing pizzeria data
 data <- read.csv("Mapping/data/csv/pizzamap.csv")
 
-## Loading the base map
-map <- leaflet(countries) %>%
-  ## Basemap
+# Initialize the leaflet map with the shapefile data
+map <- leaflet(countries) %>% 
+  ## Add the base map layer
   ##addTiles() %>%
-  ## Example of basemap
-  addProviderTiles(providers$OpenStreetMap)  %>%
-
-  ## Second example of basemap
-  ##addProviderTiles(providers$Stamen.Toner)  %>%
-
-  ## Selecting the map focus and zoom for users
-  ##setView(lng = -36.80,
-  ##      lat = 22.98,
-  ##    zoom = 1 ) %>%
-
-  ## Add the markers on the locations indicated by the lat & Lng columns with the popups
+  ## Example of a base map using OpenStreetMap tiles
+  addProviderTiles(providers$OpenStreetMap)  %>% 
+  
+  ## Another example of a base map using Stamen Toner tiles (commented out)
+  ##addProviderTiles(providers$Stamen.Toner)  %>% 
+  
+  ## Set the initial view of the map (center and zoom level)
+  setView(lng = 60.27444399596726, 
+          lat = 27.808314896631217, 
+          zoom = 2 ) %>%
+  
+  ## Add markers for each pizzeria using the latitude and longitude columns from the CSV
   addMarkers(data = data,
-             lng = ~lng,
+             lng = ~lng, 
              lat = ~lat,
              group = "Pizzerias",
              popup = ~paste("<b>", name, "</b>" ,"<br>", "<br>",
                             "City:", "<b>", city,"</b>", " in" , "<b>", country, "</b>", "<br>",
-                            "Open on:", "<b>", date_viz, "</b>", "</b>","<br>",
+                            "Open on:", "<b>", date, "</b>", "</b>","<br>",
                             "You can find here ", "<b>", pizzas, "</b>", " pizzas","<br>",
                             "The special pizza is the:", "<b>", pizza_special, "</b>", "<br>",
                             "Take away:", "<b>", take_away, "</b>", "<br>",
-                            "They sell other thins?", "<b>", other_stuff, "</b>",
+                            "They sell other things?", "<b>", other_stuff, "</b>",
                             sep = " ")) %>%
-
-  ## Add a polygon layer with label
-  addPolygons(data = countries,
-              color =	"#008000",
-              weight = 1,
+  
+  ## Add a polygon layer representing countries with a label
+  addPolygons(data = countries, 
+              color = "#008000", 
+              weight = 1, 
               smoothFactor = 0,
               group = "Countries",
-              label = ~paste(countries$titles, ": ", countries$annotation, " pizzerias"))%>%
-
-  addLegend("topright",
-            colors = c("trasparent"),
-            labels=c("Giovanni Pietro Vitali - giovannipietrovitali@gmail.com"),
-            title="Pizza Map (Learn R to make maps):") %>%
-
+              label = ~paste(countries$COUNTRY, ": ", countries$number, " pizzerias")) %>%
+  
+  ## Add a legend to the top right of the map with custom colors and labels
+  addLegend("topright", 
+            colors = c("transparent"), 
+            labels = c("Giovanni Pietro Vitali - giovannipietrovitali@gmail.com"),
+            title = "Pizza Map (Learn R to make maps):") %>%
+  
+  ## Add a mini map to the bottom left corner of the main map
   addMiniMap("bottomleft") %>%
+  
+  ## Add layer control allowing users to toggle between overlay groups
+  addLayersControl(overlayGroups = c("Pizzerias", "Countries"),
+                   options = layersControlOptions(collapsed = TRUE))
 
-  addLayersControl(overlayGroups = c("Pizzerias", "Countries"))
-options = layersControlOptions(collapsed = TRUE)
-
-
-## closing the map
+## Render the map
 map
